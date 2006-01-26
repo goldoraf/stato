a2238b4d243afdf6e53b1875a9c4b8a6ab36c02e
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-01-26 15:39:31 +0000

    Added truncate() helper

--- view/lib/helpers/texthelper.lib.php
+++ view/lib/helpers/texthelper.lib.php
@@ -5,4 +5,12 @@ function html_escape($html)
     return htmlspecialchars($html, ENT_QUOTES);
 }
 
+function truncate($text, $length = 30, $truncateString = '...')
+{
+    if (strlen($text) > $length)
+        return substr_replace($text, $truncateString, $length - strlen($truncateString));
+    else
+        return $text;
+}
+
 ?>
