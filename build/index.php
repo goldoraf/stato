a439c91da9e01dcd32e19337b1112e578f67b40d
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-01-31 15:45:25 +0000

    Modified index.php : dir constants are now correctly defined with both Windows and Linux

--- build/index.php
+++ build/index.php
@@ -2,12 +2,19 @@
 
 define('DEBUG_MODE', True);
 
+define('DOC_ROOT_DIR', str_replace('\\', '/', realpath(dirname(__FILE__).'/..')));
 define('ROOT_DIR', str_replace('\\', '/', dirname(__FILE__)));
-define('BASE_DIR', str_replace($_SERVER['DOCUMENT_ROOT'], '', ROOT_DIR));
+define('BASE_DIR', str_replace(DOC_ROOT_DIR, '', ROOT_DIR));
 define('APP_DIR', ROOT_DIR.'/app');
 
 define('SITE_HOST', $_SERVER['HTTP_HOST']);
 define('SITE_URL', 'http://'.SITE_HOST.BASE_DIR);
+
+/*echo 'DOC_ROOT : '.DOC_ROOT_DIR."\n"
+.'ROOT_DIR : '.ROOT_DIR."\n"
+.'BASE_DIR : '.BASE_DIR."\n"
+.'SITE_HOST : '.SITE_HOST."\n"
+.'SITE_URL : '.SITE_URL;*/
 
 require_once('core/common/common.php');
 require_once('core/controller/controller.php');
