669d9e5dc5e03e3edd10a4f74fcecbace82e46a6
opopope <opopope@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-02-08 16:01:51 +0000

    Removed SecurityImage class; Moved Renderer class to controller folder.

--- controller/controller.php
+++ controller/controller.php
@@ -3,6 +3,6 @@
 require_once('lib/dispatcher.class.php');
 require_once('lib/actioncontroller.class.php');
 require_once('lib/crudcontroller.class.php');
-//require_once('lib/aclcontroller.class.php');
+require_once('lib/renderer.class.php');
 
 ?>
