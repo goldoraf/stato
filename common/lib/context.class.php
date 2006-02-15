78066382f78e7dbf13c23e1f4ad698ebc208438c
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-02-15 16:51:35 +0000

    Fixed little bug : routes were not loaded before url parsing.

--- common/lib/context.class.php
+++ common/lib/context.class.php
@@ -26,11 +26,11 @@ class SContext
     {
         if (SContext::$status) return;
         
+        require_once(ROOT_DIR.'/conf/routes.php');
+        
         self::$request  = new SRequest();
         self::$response = new SResponse();
         self::$session  = new SSession();
-        
-        require_once(ROOT_DIR.'/conf/routes.php');
         
         SLocale::initialize();
         
