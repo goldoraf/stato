d3a1f37f427f16c2e8f5dcadc5aca10e8236a81e
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-02-27 16:10:40 +0000

    Refactored SRequest class and added some new methods.

--- common/lib/request.class.php
+++ common/lib/request.class.php
@@ -2,39 +2,78 @@
 
 class SRequest
 {
-    const METHOD_POST = 1;
-    const METHOD_GET  = 2;
-    
-    public $uri        = Null;
-    public $host       = Null;
     public $module     = Null;
     public $controller = Null;
     public $action     = Null;
-    public $method     = Null;
     public $params     = array();
-    public $relativeUrlRoot = Null;
+    
+    private $relativeUrlRoot = Null;
+    private $requestUri      = Null;
+    private $host            = Null;
     
     public function __construct()
     {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') $this->method = self::METHOD_POST;
-        elseif ($_SERVER['REQUEST_METHOD'] == 'GET') $this->method = self::METHOD_GET;
-        
-        $this->relativeUrlRoot = $this->relativeUrlRoot();
-        $this->uri = $this->extractPath();
-        $this->host = $_SERVER['SERVER_NAME'];
-        
         $this->parseUrl();
         $this->parseParameters();
     }
     
     public function isPost()
     {
-        return $this->method == self::METHOD_POST;
+        return $_SERVER['REQUEST_METHOD'] == 'POST';
     }
     
     public function isGet()
     {
-        return $this->method == self::METHOD_GET;
+        return $_SERVER['REQUEST_METHOD'] == 'GET';
+    }
+    
+    public function isSSL()
+    {
+        return $_SERVER['HTTPS'] == 'on';
+    }
+    
+    public function host()
+    {
+        return $_SERVER['SERVER_NAME'];
+    }
+    
+    public function port()
+    {
+        return $_SERVER['SERVER_PORT'];
+    }
+    
+    public function protocol()
+    {
+        return ($this->isSSL() ? 'https://' : 'http://');
+    }
+    
+    public function standardPort()
+    {
+        return (($this->protocol() == 'https://') ? 443 : 80);
+    }
+    
+    public function portString()
+    {
+        return (($this->port() == $this->standardPort()) ? '' : ':'.$this->port());
+    }
+    
+    public function hostWithPort()
+    {
+        return $this->host().$this->portString();
+    }
+    
+    public function requestUri()
+    {
+        if (!isset($this->requestUri))
+            $this->requestUri = substr($_SERVER['REQUEST_URI'], strlen($this->relativeUrlRoot()));
+        return $this->requestUri;
+    }
+    
+    public function relativeUrlRoot()
+    {
+        if (!isset($this->relativeUrlRoot))
+            $this->relativeUrlRoot = str_replace('/index.php', '/', $_SERVER['SCRIPT_NAME']);
+        return $this->relativeUrlRoot;
     }
     
     private function parseUrl()
@@ -44,16 +83,6 @@ class SRequest
         $this->controller = $options['controller'];
         $this->action     = $options['action'];
         $this->params     = $options;
-    }
-    
-    private function extractPath()
-    {
-        return substr($_SERVER['REQUEST_URI'], strlen($this->relativeUrlRoot));
-    }
-    
-    private function relativeUrlRoot()
-    {
-        return str_replace('/index.php', '/', $_SERVER['SCRIPT_NAME']);
     }
     
     private function parseParameters()
