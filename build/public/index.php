7141d33eee338ccdbd37aeb6112383a0916e0b80
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-02-28 13:25:27 +0000

    Modified /build/public/index.php : controller classes are now included before common classes

--- build/public/index.php
+++ build/public/index.php
@@ -1,23 +1,10 @@
 <?php
 
 define('APP_MODE', 'dev');
+define('ROOT_DIR', str_replace('\\', '/', realpath(dirname(__FILE__).'/..')));
 
-define('DOC_ROOT_DIR', str_replace('\\', '/', realpath(dirname(__FILE__).'/..')));
-define('ROOT_DIR', str_replace('\\', '/', dirname(__FILE__)));
-define('BASE_DIR', str_replace(DOC_ROOT_DIR, '', ROOT_DIR));
-define('APP_DIR', ROOT_DIR.'/app');
-
-define('SITE_HOST', $_SERVER['HTTP_HOST']);
-define('SITE_URL', 'http://'.SITE_HOST.BASE_DIR);
-
-/*echo 'DOC_ROOT : '.DOC_ROOT_DIR."\n"
-.'ROOT_DIR : '.ROOT_DIR."\n"
-.'BASE_DIR : '.BASE_DIR."\n"
-.'SITE_HOST : '.SITE_HOST."\n"
-.'SITE_URL : '.SITE_URL;*/
-
-require_once('core/common/common.php');
-require_once('core/controller/controller.php');
+require_once('../core/controller/controller.php');
+require_once('../core/common/common.php');
 
 $controller = new SDispatcher();
 $controller->dispatch();
