595d6a6c5b116425b096af0635236f6c921b6bc9
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-02-28 13:32:17 +0000

    Moved all helpers in a /helpers dir

--- controller/controller.php
+++ controller/controller.php
@@ -3,6 +3,5 @@
 require_once('lib/dispatcher.class.php');
 require_once('lib/actioncontroller.class.php');
 require_once('lib/crudcontroller.class.php');
-require_once('lib/renderer.class.php');
 
 ?>
--- view/view.php
+++ view/view.php
@@ -2,18 +2,18 @@
 
 require_once('lib/actionview.class.php');
 
-require_once('lib/taghelper.lib.php');
-require_once('lib/urlhelper.lib.php');
-require_once('lib/formtaghelper.lib.php');
-require_once('lib/formoptionshelper.lib.php');
-require_once('lib/formhelper.lib.php');
-require_once('lib/datehelper.lib.php');
-require_once('lib/pagerhelper.lib.php');
-require_once('lib/ajaxhelper.lib.php');
-require_once('lib/recordhelper.lib.php');
-require_once('lib/partialshelper.lib.php');
-require_once('lib/texthelper.lib.php');
-require_once('lib/utf8helper.lib.php');
-require_once('lib/cachehelper.lib.php');
+require_once('lib/helpers/taghelper.lib.php');
+require_once('lib/helpers/urlhelper.lib.php');
+require_once('lib/helpers/formtaghelper.lib.php');
+require_once('lib/helpers/formoptionshelper.lib.php');
+require_once('lib/helpers/formhelper.lib.php');
+require_once('lib/helpers/datehelper.lib.php');
+require_once('lib/helpers/pagerhelper.lib.php');
+require_once('lib/helpers/ajaxhelper.lib.php');
+require_once('lib/helpers/recordhelper.lib.php');
+require_once('lib/helpers/partialshelper.lib.php');
+require_once('lib/helpers/texthelper.lib.php');
+require_once('lib/helpers/utf8helper.lib.php');
+require_once('lib/helpers/cachehelper.lib.php');
 
 ?>
