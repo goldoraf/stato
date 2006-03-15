8258e5bae08932d9f63a921e1f4a0c3d81e48a92
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-03-15 11:06:56 +0000

    Added button_to() helper

--- view/lib/helpers/urlhelper.lib.php
+++ view/lib/helpers/urlhelper.lib.php
@@ -14,6 +14,23 @@ function link_to($label, $urlOptions=array(), $htmlOptions=array())
     return content_tag('a', $label, array_merge(array('href' => $url), $htmlOptions));
 }
 
+function button_to($label, $urlOptions=array(), $htmlOptions=array())
+{
+    if (is_array($urlOptions)) $url = url_for($urlOptions);
+    else $url = html_escape($urlOptions);
+    
+    if (isset($htmlOptions['confirm']))
+    {
+        $htmlOptions['onclick'] = "input=confirm('{$htmlOptions['confirm']}');return input;";
+        unset($htmlOptions['confirm']);
+    }
+    
+    $htmlOptions = array_merge(array('type'=>'submit', 'value'=>$label), $htmlOptions);
+    
+    return "<form method=\"post\" action=\"{$url}\" class=\"button-to\"><div>"
+    .tag('input', $htmlOptions)."</div></form>";
+}
+
 function url_for($options)
 {
     return SUrlRewriter::urlFor($options);
