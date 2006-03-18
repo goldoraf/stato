94a5cc925bddcf6861ed58b961c36837919f4837
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-03-18 15:26:22 +0000

    Renamed /common and /controller classes files ; moved some classes from /common to /controller

--- common/common.php
+++ common/common.php
@@ -1,17 +1,10 @@
 <?php
 
-require_once('lib/types.class.php');
-require_once('lib/request.class.php');
-require_once('lib/response.class.php');
-require_once('lib/upload.class.php');
-require_once('lib/session.class.php');
-require_once('lib/flash.class.php');
-require_once('lib/locale.class.php');
-require_once('lib/routes.class.php');
-require_once('lib/urlrewriter.class.php');
-require_once('lib/encryption.class.php');
-require_once('lib/logger.class.php');
-require_once('lib/fileutils.class.php');
-require_once('lib/inflection.class.php');
+require_once('lib/types.php');
+require_once('lib/locale.php');
+require_once('lib/encryption.php');
+require_once('lib/logger.php');
+require_once('lib/fileutils.php');
+require_once('lib/inflection.php');
 
 ?>
--- controller/controller.php
+++ controller/controller.php
@@ -1,7 +1,15 @@
 <?php
 
-require_once('lib/dispatcher.class.php');
-require_once('lib/actioncontroller.class.php');
-require_once('lib/crudcontroller.class.php');
+require_once('lib/action_controller.php');
+require_once('lib/crud_controller.php');
+require_once('lib/dispatcher.php');
+require_once('lib/cookie.php');
+require_once('lib/flash.php');
+require_once('lib/request.php');
+require_once('lib/response.php');
+require_once('lib/routes.php');
+require_once('lib/session.php');
+require_once('lib/upload.php');
+require_once('lib/url_rewriter.php');
 
 ?>
