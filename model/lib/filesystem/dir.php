f75097a86adfab764d1e6d9ef8b3932be84d2b3f
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-10-03 15:59:45 +0000

    Added SDir::copy() (recursive !)

--- model/lib/filesystem/dir.php
+++ model/lib/filesystem/dir.php
@@ -11,6 +11,37 @@ class SDir
     {
         return is_dir($path);
     }
+    
+    public static function unlink($path)
+    {
+        return unlink($path);
+    }
+    
+    public static function copy($source, $dest, $overwrite = false)
+    {
+        if (!self::exists($dest))
+            throw new SException('Destination directory does not exist.');
+            
+        $dir = new DirectoryIterator($source);
+        foreach ($dir as $file)
+        {
+            if ($file->isDot()) continue;
+            if ($file->isFile()) 
+            {
+                if (!is_file($dest.'/'.$file->getFilename()) || $overwrite)
+                {
+                    if (!@copy($source.'/'.$file->getFilename(), $dest.'/'.$file->getFilename()))
+                        throw new SException('File '.$file->getFilename().' could not be copied.');
+                }
+            }
+            elseif ($file->isDir())
+            {
+                if (!self::exists($dest.'/'.$file->getFilename()))
+                    self::mkdir($dest.'/'.$file->getFilename());
+                self::copy($source.'/'.$file->getFilename(), $dest.'/'.$file->getFilename());
+            }
+        }
+    }
 }
 
 ?>
