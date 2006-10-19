2299a5aa3377a481e6935401269779690709c022
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-10-19 13:43:20 +0000

    Modified model generation script

--- cli/generate.php
+++ cli/generate.php
@@ -12,32 +12,24 @@ if ($_SERVER['argc'] == 3)
     {
         case 'model':
             if (strpos($_SERVER['argv'][2], '/') !== false)
-                list($subdir, $assigns['className']) = explode('/', $_SERVER['argv'][2]);
+                list($subdir, $className) = explode('/', $_SERVER['argv'][2]);
             else
-                $assigns['className'] = $_SERVER['argv'][2];
+                $className = $_SERVER['argv'][2];
                 
-            $content = SCodeGenerator::renderTemplate(CORE_DIR.'/cli/lib/templates/model.txt', $assigns);
-            $file = SInflection::underscore($assigns['className']).'.php';
+            $content = SCodeGenerator::generateClass($className, '    public static $objects;', 'SActiveRecord');
+            $file = SInflection::underscore($className).'.php';
             if (!empty($subdir)) $file = $subdir.'/'.$file;
             $path = ROOT_DIR.'/app/models/'.$file;
             if (file_exists($path))
             {
                 echo "WARNING : file $path already exists !\n"
-                .'Do you want to overwrite (o), update (u) or abort (a) ? ';
+                .'Do you want to overwrite (o), or abort (a) ? ';
                 $answer = fgetc(STDIN);
                 if ($answer == 'a')
                 {
                     echo "\nFile generation aborted.\n";
                     die();
                 }
-                elseif ($answer == 'u')
-                    $content = SCodeGenerator::insertCode(file_get_contents($path), $content);
-                else
-                    $content = SCodeGenerator::generateClass($assigns['className'], $content);
-            }
-            else
-            {
-                $content = SCodeGenerator::generateClass($assigns['className'], $content, 'SActiveRecord');
             }
             file_put_contents($path, $content);
             break;
