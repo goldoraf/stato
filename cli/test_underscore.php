2d1b213399ef3d29844859dccf4c4b4d1b6da0bf
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-11-12 14:20:58 +0000

    Added new script to convert classes from camel case style to underscore style

---
+++ cli/test_underscore.php
@@ -0,0 +1,61 @@
+<?php
+
+define('ROOT_DIR', str_replace('\\', '/', realpath(dirname(__FILE__).'/../..')));
+define('CORE_DIR', ROOT_DIR.'/core');
+define('APP_DIR', ROOT_DIR.'/app');
+
+require_once(CORE_DIR.'/common/lib/inflection.php');
+
+$files_to_exclude = array('inflection.php', 'dir.php', 'colortextreporter.php', 
+'helpertestcase.php', 'showpasses.php', 'statotestcase.php');
+
+$dir = new RecursiveDirectoryIterator(CORE_DIR);
+iterate_files($dir, array('build'));
+
+function iterate_files($dir, $exceptions)
+{
+    foreach ($dir as $file)
+    {
+        if ($file->hasChildren() && !in_array((string) $file, $exceptions))
+        {
+            echo "Opening $file\n";
+            iterate_files($file->getChildren(), $exceptions);
+        }
+        elseif (substr($file, -4) == '.php')
+        {
+            $path = $dir->getPath()."/$file";
+            echo "Processing $path\n";
+            underscore_file($path);
+        }
+    }
+}
+
+function underscore_file($class_file)
+{
+    $regexes = array
+    (
+        '/(\$[a-zA-Z0-9]+)/i',
+        '/(->[a-zA-Z0-9]+)/i',
+        '/(function [a-zA-Z0-9]+)/i',
+        '/(::[a-zA-Z0-9]+)/i'
+    );
+    
+    $code = file_get_contents($class_file);
+    $new_code = preg_replace_callback($regexes, 'underscore', $code);
+    
+    file_put_contents($class_file, $new_code);
+}
+
+function underscore($matches)
+{
+    $exceptions = array('->assertEqual', '->assertNotEqual', '->assertDomEqual', '->assertException', '->assertNull', '->assertTrue', '->assertFalse', 
+    '->assertIsA', 'function offsetExists', 'function offsetSet', 'function offsetGet', 'function offsetUnset', 'function __toString', 
+    '->__toString', '$_POST', '$_GET', '$_FILES', '$_SERVER');
+    
+    if (!in_array($matches[1], $exceptions)) return SInflection::underscore($matches[1]);
+    else return $matches[1];
+}
+
+//underscore_file(ROOT_DIR.'/core/model/test/active_record.test.php');
+
+?>
