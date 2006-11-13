18d348dc7bd2f2636c295fd47270c40521afe3af
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-11-13 10:39:18 +0000

    Modified underscore.php script in order to use DirectoryIterator instead of RecursiveDirectoryIterator

--- cli/underscore.php
+++ cli/underscore.php
@@ -6,10 +6,10 @@ define('APP_DIR', ROOT_DIR.'/app');
 
 require_once(CORE_DIR.'/common/lib/inflection.php');
 
-$dir = new RecursiveDirectoryIterator(CORE_DIR);
+/*$dir = new RecursiveDirectoryIterator(APP_DIR);
 iterate_files($dir, array('build'));
 
-function iterate_files($dir, $exceptions)
+function iterate_files($dir, $exceptions = array())
 {
     $files_to_exclude = array('inflection.php', 'dir.php', 'colortextreporter.php', 
     'helpertestcase.php', 'showpasses.php', 'statotestcase.php', 'run_tests.php', 'test_underscore.php', 'test_underscore2.php');
@@ -26,6 +26,31 @@ function iterate_files($dir, $exceptions)
             $path = $dir->getPath()."/$file";
             echo "Processing $path\n";
             underscore_file($path);
+        }
+    }
+}*/
+
+iterate_files(APP_DIR);
+
+function iterate_files($path, $exceptions = array())
+{
+    $files_to_exclude = array('inflection.php', 'dir.php', 'colortextreporter.php', 
+    'helpertestcase.php', 'showpasses.php', 'statotestcase.php', 'run_tests.php', 'test_underscore.php', 'test_underscore2.php');
+    
+    $dir = new DirectoryIterator($path);
+    
+    foreach ($dir as $file)
+    {
+        if ($file->isDir() && !$file->isDot() && (string) $file != '.svn' && !in_array((string) $file, $exceptions))
+        {
+            echo "Opening $file\n";
+            iterate_files($path.'/'.$file->getFilename(), $exceptions);
+        }
+        elseif (substr($file, -4) == '.php' && !in_array((string) $file, $files_to_exclude))
+        {
+            $filepath = $file->getPath()."/$file";
+            echo "Processing $filepath\n";
+            underscore_file($filepath);
         }
     }
 }
@@ -51,9 +76,11 @@ function underscore($matches)
 {
     $exceptions = array('->assertEqual', '->assertNotEqual', '->assertDomEqual', '->assertException', '->assertNull', '->assertNotNull', '->assertTrue', '->assertFalse', 
     '->assertIsA', '->assertNothingThrown', 'function offsetExists', 'function offsetSet', 'function offsetGet', 'function offsetUnset', 'function __toString', 
-    '->__toString', '$_POST', '$_GET', '$_FILES', '$_SERVER', '$GLOBALS', '::UnitTestCase', '->hasProperty', '::CSV_MODE', '::INI_MODE',
+    '->__toString', '$_POST', '$_GET', '$_FILES', '$_SERVER', '$GLOBALS', '$_SESSION', '::UnitTestCase', '->hasProperty', '::CSV_MODE', '::INI_MODE',
     '->getParentClass', '->getName', '->setStaticPropertyValue', '->getStaticPropertyValue', 'function setUp', 'function tearDown',
-    '->assertCopy', '->isPublic', '->isConstructor', '->getDeclaringClass');
+    '->assertCopy', '->isPublic', '->isConstructor', '->getDeclaringClass', '->getElementsByTagName', '->hasAttribute', '->getAttribute',
+    '->setAttribute', '->firstChild', '::loadXml', '->importNode', '->parentNode', '->replaceChild', '->hasChildNodes', '->childNodes', '->nodeName',
+    '->documentElement');
     
     if (!in_array($matches[1], $exceptions)) return SInflection::underscore($matches[1]);
     else return $matches[1];
