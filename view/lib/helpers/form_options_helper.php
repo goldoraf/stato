0072141ff98191288305309526aa6dc7333b44c3
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-12-14 11:06:13 +0000

    Added comments to select(), collection_select() and options_for_select() helpers

--- view/lib/helpers/form_options_helper.php
+++ view/lib/helpers/form_options_helper.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * Create a select tag and a series of contained option tags for the provided object and method. 
+ * The option currently held by the object will be selected.
+ * See options_for_select for the required format of the <var>$choices</var> argument. 
+ * 
+ * Example :
+ * <code>
+ * select('post', 'category', Post::$categories, array('include_blank' => True));
+ * </code>
+ * could generate :
+ * <code>
+ * <select id="post_category" name="post[category]">
+ *   <option></option>
+ *   <option>PHP</option>
+ *   <option>Linux</option>
+ *   <option>Space</option>
+ * </select>
+ * </code>
+ */
 function select($object_name, $method, $object, $choices, $options = array(), $html_options = array())
 {
     list($name, $value, $html_options) = default_options($object_name, $method, $object, $html_options);
@@ -7,7 +26,9 @@ function select($object_name, $method, $object, $choices, $options = array(), $h
     return select_tag($name, $options_block, $html_options);
 }
 
-
+/**
+ * Return select and option tags for the given object and method using options_from_collection_for_select to generate the list of option tags.
+ */
 function collection_select($object_name, $method, $object, $collection, $value_prop='id', $text_prop=null, $options=array(), $html_options = array())
 {
     list($name, $value, $html_options) = default_options($object_name, $method, $object, $html_options);
@@ -23,6 +44,23 @@ function add_select_options($options_block, $options, $value = null)
     return $options_block;
 }
 
+/**
+ * Accepts a an array and returns a string of option tags.
+ * 
+ * Examples :  
+ * <code>
+ * options_for_select(array('Margharita', 'Calzone', 'Napolitaine'));
+ * // returns :
+ * <option value="Margharita">Margharita</option>
+ * <option value="Calzone">Calzone</option>
+ * <option value="Napolitaine">Napolitaine</option>
+ *  
+ * options_for_select(array('Margharita'=>'7€', 'Calzone'=>'9€', 'Napolitaine'=>'8€'));
+ * // returns :
+ * <option value="7€">Margharita</option>
+ * <option value="9€">Calzone</option>
+ * <option value="8€">Napolitaine</option> 
+ */
 function options_for_select($set, $selected = null, $detect_non_associative = true)
 {
     $str = '';
@@ -52,59 +90,6 @@ function options_from_collection_for_select($collection, $value_prop='id', $text
 function options_groups_from_collection_for_select()
 {
 
-}
-
-function country_options_for_select()
-{
-    return array("Afghanistan", "Albania", "Algeria", "American Samoa", 
-    "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua And Barbuda", 
-    "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", 
-    "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", 
-    "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina",
-    "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", 
-    "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burma", "Burundi", 
-    "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", 
-    "Central African Republic", "Chad", "Chile", "China", "Christmas Island", 
-    "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", 
-    "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", 
-    "Cote d'Ivoire", "Croatia", "Cyprus", "Czech Republic", "Denmark", 
-    "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", 
-    "Egypt", "El Salvador", "England", "Equatorial Guinea", "Eritrea", 
-    "Espana", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", 
-    "Fiji", "Finland", "France", "French Guiana", "French Polynesia", 
-    "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", 
-    "Ghana", "Gibraltar", "Great Britain", "Greece", "Greenland", "Grenada", 
-    "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", 
-    "Haiti", "Heard and Mc Donald Islands", "Honduras", "Hong Kong", "Hungary",
-    "Iceland", "India", "Indonesia", "Ireland", "Israel", "Italy", "Iran", 
-    "Irak", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", 
-    "Korea, Republic of", "Korea (South)", "Kuwait", "Kyrgyzstan", 
-    "Lao People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", 
-    "Liberia", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", 
-    "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", 
-    "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", 
-    "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", 
-    "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", 
-    "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", 
-    "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Ireland", 
-    "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", 
-    "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", 
-    "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", 
-    "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", 
-    "Samoa (Independent)", "San Marino", "Sao Tome and Principe", "Saudi Arabia", 
-    "Scotland", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", 
-    "Slovenia", "Solomon Islands", "Somalia", "South Africa", 
-    "South Georgia and the South Sandwich Islands", "South Korea", "Spain", 
-    "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Suriname", 
-    "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", 
-    "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tokelau", "Tonga", 
-    "Trinidad", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", 
-    "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", 
-    "United Arab Emirates", "United Kingdom", "United States", 
-    "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", 
-    "Vatican City State (Holy See)", "Venezuela", "Viet Nam", 
-    "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wales", 
-    "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Zambia", "Zimbabwe");
 }
 
 ?>
