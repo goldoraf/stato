a48b0c2c591a728137ffc5b077fcfe5000a5f7cd
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-12-15 09:09:58 +0000

    SDir class now extends DirectoryIterator

--- common/lib/dir.php
+++ common/lib/dir.php
@@ -1,7 +1,36 @@
 <?php
 
-class SDir
+class SDir extends DirectoryIterator
 {
+    public function __construct($path)
+    {
+        parent::__construct($path);
+    }
+    
+    public function current()
+    {
+        return parent::get_file_name();
+    }
+    
+    public function valid()
+    {
+        if (parent::valid())
+        {
+            if (!parent::is_file())
+            {
+                parent::next();
+                return $this->valid();
+            }
+            return True;
+        }
+        return False;
+    }
+    
+    public function rewind()
+    {
+        parent::rewind();
+    }
+    
     public static function mkdir($path, $mode = 0777)
     {
         return mkdir($path, $mode);
