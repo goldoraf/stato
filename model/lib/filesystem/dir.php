fa27cd8b95b029213b80c3af99a0121ce11b24f2
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2006-12-15 09:01:27 +0000

    Moved rmdirs() and mkdirs() recursive methods from SFileUtils to SDir

--- model/lib/filesystem/dir.php
+++ model/lib/filesystem/dir.php
@@ -17,6 +17,22 @@ class SDir
         return unlink($path);
     }
     
+    public static function mkdirs($dir, $mode = 0777, $recursive = true)
+    {
+        if (is_null($dir) || $dir === "") return false;
+        if (is_dir($dir) || $dir === "/") return true;
+        if (self::mkdirs(dirname($dir), $mode, $recursive)) return mkdir($dir, $mode);
+        return false;
+    }
+    
+    public static function rmdirs($path)
+    {   
+        $dir = new RecursiveDirectoryIterator($path);
+        foreach (new RecursiveIteratorIterator($dir) as $file) unlink($file);
+        foreach ($dir as $subDir) if(!@rmdir($subDir)) self::rmdirs($subDir);
+        rmdir($path);
+    }
+    
     public static function copy($source, $dest, $overwrite = false)
     {
         if (!self::exists($dest))
