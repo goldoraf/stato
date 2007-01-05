8393e7fa51e7d3ad3714ef4ee5d219599afa9c27
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2007-01-05 08:51:07 +0000

    Fixed notice thrown by asset_tag_helper.php

--- view/lib/helpers/asset_tag_helper.php
+++ view/lib/helpers/asset_tag_helper.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!defined(JAVASCRIPT_DEFAULT_SOURCES))
+if (!defined('JAVASCRIPT_DEFAULT_SOURCES'))
     define('JAVASCRIPT_DEFAULT_SOURCES', 'prototype;controls;dragdrop;effects;lowpro');
 
 function image_tag($filename, $options = array())
