de0b3ce0ca01321f2881c6dcd7de5e90d86e56a6
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2007-01-15 14:19:37 +0000

    Moved XMLRPC classes in /xmlrpc dir

--- webservice/webservice.php
+++ webservice/webservice.php
@@ -5,7 +5,8 @@ require_once('lib/api.php');
 require_once('lib/struct.php');
 require_once('lib/web_service.php');
 require_once('lib/http_client.php');
-require_once('lib/xmlrpc_client.php');
-require_once('lib/xmlrpc_server.php');
+require_once('lib/xmlrpc/client.php');
+require_once('lib/xmlrpc/server.php');
+require_once('lib/xmlrpc/value.php');
 
 ?>
