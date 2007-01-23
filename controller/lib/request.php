fd2d9e498e26841fe0e4c02ca18bc015853d98ab
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2007-01-23 18:05:58 +0000

    Added comments to SRequest class

--- controller/lib/request.php
+++ controller/lib/request.php
@@ -1,91 +1,153 @@
 <?php
 
+/**
+ * Request class
+ * 
+ * @package Stato
+ * @subpackage controller
+ */
 class SRequest
 {
-    public $controller = Null;
-    public $action     = Null;
-    public $params     = array();
+    /**
+     * Returns the requested controller name
+     */
+    public $controller = null;
+    /**
+     * Returns the requested action name
+     */
+    public $action = null;
+    /**
+     * Holds both GET and POST parameters in a single array. Uploaded files are held 
+     * in an instance of SUpload class     
+     */
+    public $params = array();
     
-    private $relative_url_root = Null;
-    private $request_uri      = Null;
-    private $host            = Null;
+    private $relative_url_root = null;
+    private $request_uri       = null;
+    private $host              = null;
     
     public function __construct()
     {
         if ($this->is_post()) $this->parse_post_parameters();
     }
     
+    /**
+     * Is this a POST request ?
+     */
     public function is_post()
     {
         return $this->method() == 'POST';
     }
     
+    /**
+     * Is this a GET request ?
+     */
     public function is_get()
     {
         return $this->method() == 'GET';
     }
     
+    /**
+     * Is this an SSL request ?
+     */
     public function is_ssl()
     {
         return @$_SERVER['HTTPS'] == 'on';
     }
     
+    /**
+     * Returns true if the request's "X-Requested-With" header contains "XMLHttpRequest".
+     * (The Prototype Javascript library sends this header with every Ajax request.)
+     */
     public function is_xml_http_request()
     {
         if (preg_match('/XMLHttpRequest/i', @$_SERVER['HTTP_X_REQUESTED_WITH'])) return true;
         return false;
     }
     
+    /**
+     * Alias for is_xml_http_request()
+     */
     public function is_xhr()
     {
         return $this->is_xml_http_request();
     }
     
+    /**
+     * Returns the HTTP request method
+     */
     public function method()
     {
         return $_SERVER['REQUEST_METHOD'];
     }
     
+    /**
+     * Returns the host for this request
+     */
     public function host()
     {
         return $_SERVER['SERVER_NAME'];
     }
     
+    /**
+     * Returns the port number of this request
+     */
     public function port()
     {
         return $_SERVER['SERVER_PORT'];
     }
     
+    /**
+     * Determine originating IP address, using REMOTE_ADDR header
+     */
     public function remote_ip()
     {
         return $_SERVER['REMOTE_ADDR'];
     }
     
+    /**
+     * Return 'https://' if this is an SSL request and 'http://' otherwise
+     */
     public function protocol()
     {
         return ($this->is_ssl() ? 'https://' : 'http://');
     }
     
+    /**
+     * Returns the standard port number for this request's protocol
+     */
     public function standard_port()
     {
         return (($this->protocol() == 'https://') ? 443 : 80);
     }
     
+    /**
+     * Returns a port suffix like ":8080" if the port number of this request is not the default HTTP port 80 or HTTPS port 443
+     */
     public function port_string()
     {
         return (($this->port() == $this->standard_port()) ? '' : ':'.$this->port());
     }
     
+    /**
+     * Returns a host:port string for this request
+     */
     public function host_with_port()
     {
         return $this->host().$this->port_string();
     }
     
+    /**
+     * Returns the raw post data
+     */
     public function raw_post_data()
     {
         return file_get_contents('php://input');
     }
     
+    /**
+     * Return the request URI
+     */
     public function request_uri()
     {
         if (!isset($this->request_uri))
@@ -93,6 +155,9 @@ class SRequest
         return $this->request_uri;
     }
     
+    /**
+     * Returns the path minus the web server relative installation directory
+     */
     public function relative_url_root()
     {
         if (!isset($this->relative_url_root))
