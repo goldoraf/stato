997cda429f9c6419dd7d845c1ac68b3086d2b77a
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2007-01-24 14:01:05 +0000

    Added comments to i18n helpers

--- view/lib/helpers/i18n_helper.php
+++ view/lib/helpers/i18n_helper.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * I18n helpers
+ * 
+ * @package Stato
+ * @subpackage view
+ */
+/**
+ * Returns translation corresponding to <var>$key</var>.
+ */
 function __($key)
 {
     return SLocale::translate($key);
