c07211707bff2a55c441bced1d2d1a1addf100d4
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2007-02-01 17:42:48 +0000

    Added __call overloading to StatoTestCase class in order to permit underscored methods calls (ie assert_true instead of assertTrue)

--- cli/lib/testing/stato_test_case.php
+++ cli/lib/testing/stato_test_case.php
@@ -6,6 +6,14 @@ class StatoTestCase extends UnitTestCase
     {
         return $this->assertTrue(true);
     }
+    
+    public function __call($method, $args)
+    {
+        $method = SInflection::camelize($method);
+        if (!method_exists($this, $method))
+            throw new Exception('Tried to call unknown method '.get_class($this).'::'.$method);
+        return call_user_func_array(array($this, $method), $args);
+    }
 }
 
 ?>
