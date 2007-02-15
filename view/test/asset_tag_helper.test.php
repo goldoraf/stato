deb15a2a27fcefc94b7ec5087fbc21377dc5fada
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2007-02-15 11:23:13 +0000

    Fixed asset_tag_helper.test.php

--- view/test/asset_tag_helper.test.php
+++ view/test/asset_tag_helper.test.php
@@ -39,9 +39,9 @@ class AssetTagHelperTest extends HelperTestCase
         $this->assertDomEqual(
             javascript_include_defaults(),
             '<script src="/js/prototype.js" type="text/javascript"></script>
+            <script src="/js/effects.js" type="text/javascript"></script>
             <script src="/js/controls.js" type="text/javascript"></script>
             <script src="/js/dragdrop.js" type="text/javascript"></script>
-            <script src="/js/effects.js" type="text/javascript"></script>
             <script src="/js/lowpro.js" type="text/javascript"></script>'
         );
         $this->assertDomEqual(
