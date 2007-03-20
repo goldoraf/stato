a82dd0bb8f95926ea46cbc03c41f6ce6a6a30401
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2007-03-20 11:02:00 +0000

    Added possibility of default active record manager overriding

--- model/lib/mapper.php
+++ model/lib/mapper.php
@@ -7,7 +7,8 @@ class SMapper
     public static function add_manager_to_class($class)
     {
         $ref = new ReflectionClass($class);
-        if ($ref->hasProperty('objects')) $ref->setStaticPropertyValue('objects', new SManager($class));
+        if ($ref->hasProperty('objects')) 
+            $ref->setStaticPropertyValue('objects', self::retrieve_manager($class));
     }
     
     public static function retrieve($class)
@@ -16,6 +17,13 @@ class SMapper
             self::$cache[$class] = new STableMap($class);
         
         return self::$cache[$class];
+    }
+    
+    public static function retrieve_manager($class)
+    {
+        $manager_class = $class.'Manager';
+        if (class_exists($manager_class, false)) return new $manager_class($class);
+        return new SManager($class);
     }
     
     public static function reset_meta_information($class)
