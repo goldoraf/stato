c7ac6baedffc69f50a91683b553fcb75481a538e
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2007-11-12 15:49:00 +0000

    Enhanced SPresenter class

--- gemini/lib/presenter.php
+++ gemini/lib/presenter.php
@@ -2,16 +2,25 @@
 
 class SPresenter
 {
+    protected $record;
+    
+    public function __construct($record)
+    {
+        $this->record = $record;
+    }
+    
     public function __get($name)
     {
         $acc_method = 'get_'.$name;
         if (method_exists($this, $acc_method)) return $this->$acc_method();
+        return $this->record->$name;
     }
     
     public function __set($name, $value)
     {
         $acc_method = 'set_'.$name;
         if (method_exists($this, $acc_method)) return $this->$acc_method($value);
+        $this->record->$name = $value;
     }
 }
 
