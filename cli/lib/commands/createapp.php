1f5ca06fdda3ef88055d6ee121a42a4d0fbb2030
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2007-12-10 10:16:59 +0000

    Fixed 'compile' option of createapp command which was broken because of frameworks reorganization

--- cli/lib/commands/createapp.php
+++ cli/lib/commands/createapp.php
@@ -5,9 +5,9 @@ class CreateAppCommand extends SCommand
     protected $allowed_options = array('path' => true, 'compile' => false);
     protected $allowed_params  = array('project_name' => true);
     
-    private $project_dirs     = array('app', 'cache', 'conf', 'db', 'lib', 'log', 'public', 'scripts', 'test');
-    private $project_app_dirs = array('controllers', 'helpers', 'i18n', 'models', 'views', 'apis');
-    private $core_layers      = array('common', 'controller', 'mailer', 'model', 'view', 'webservice');
+    private $project_dirs      = array('app', 'cache', 'conf', 'db', 'lib', 'log', 'public', 'scripts', 'test');
+    private $project_app_dirs  = array('controllers', 'helpers', 'i18n', 'models', 'views', 'apis');
+    private $compilable_layers = array('common', 'gemini', 'mercury');
     
     public function execute()
     {
@@ -50,6 +50,10 @@ class CreateAppCommand extends SCommand
             $project_core_path = "$project_path/core";
             $this->create_dir("core", $project_path);
             $this->compile($project_core_path);
+            $this->create_dir('components', $project_core_path);
+            $this->create_dir('vendor', $project_core_path);
+            $this->copy(STATO_CORE_PATH.'/components', 'components', $project_core_path);
+            $this->copy(STATO_CORE_PATH.'/vendor', 'vendor', $project_core_path);
         }
         
         $this->create_file("conf/boot.php", $project_path,
@@ -62,7 +66,7 @@ class CreateAppCommand extends SCommand
     
     private function compile($core_path)
     {
-        foreach ($this->core_layers as $layer)
+        foreach ($this->compilable_layers as $layer)
         {
             $source_dir = STATO_CORE_PATH."/$layer/lib";
             $this->create_dir($layer, $core_path);
