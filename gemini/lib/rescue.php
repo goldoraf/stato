5f6253851c000e86f3e3afb5b433000ed6630012
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2008-02-06 13:40:05 +0000

    Fixed bug in error handling

--- gemini/lib/rescue.php
+++ gemini/lib/rescue.php
@@ -3,19 +3,8 @@
 function error_handler($error_type, $message)
 {
     if ($error_type == E_USER_ERROR && preg_match('/^Missing ([a-zA-Z0-9_]*) model$/', $message))
-    {
-        $exception = new SDependencyNotFound($message, $error_type);
-        
-        if (defined('STDIN') && defined('STDOUT') && defined('STDERR')) // CLI environment
-            throw $exception;
-        else
-        {
-            $dispatcher = new SDispatcher();
-            $dispatcher->rescue($exception)->out();
-        }
-        
-        die();
-    }
+        throw new SDependencyNotFound($message, $error_type);
+    
     // No exception thrown for notices : Stato uses some PHP4 librairies
     // and we don't want to bother with "var is deprecated"
     // Ideally, we could log this type of errors
