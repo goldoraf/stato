a84b6293387fd8c2b293705b91123ddbf3d0c477
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2008-02-17 14:41:22 +0000

    Added $authentication property to SResource class

--- gemini/lib/resource.php
+++ gemini/lib/resource.php
@@ -2,21 +2,28 @@
 
 class SUnknownResourceException extends Exception {}
 class SHttpMethodNotImplemented extends Exception {}
+class SUnknownAuthenticationMethod extends Exception {}
 
 class SResource
 {
+    const HTTP_BASIC = 1;
+    
     protected $request;
     protected $response;
     protected $params;
     protected $format;
     protected $mimetype;
+    protected $authentication;
+    protected $authentication_callback;
     protected $accepted_formats = array('xml', 'json');
     
-    public static function instantiate($name)
+    public static function instantiate($name, $module = null)
     {
+        if (file_exists($path = STATO_APP_PATH."/resources/base_resource.php"))
+            require_once($path);
+        
         $class_name = SInflection::camelize($name).'Resource';
-        $file_path = STATO_APP_PATH."/resources/{$name}_resource.php";
-        if (!file_exists($file_path))
+        if (!file_exists($file_path = self::resource_file($name, $module)))
             throw SUnknownResourceException("$class_name not found !");
         require_once($file_path);
         return new $class_name();
@@ -29,6 +36,9 @@ class SResource
         $this->params   =& $this->request->params;
         $this->format   = $this->request->format();
         $this->mimetype = SMimeType::lookup($this->format);
+        
+        if ($this->authentication !== null && !$this->authenticate()) 
+            return $this->response;
         
         $method = $this->request->method();
         if (!method_exists($this, $method))
@@ -43,6 +53,19 @@ class SResource
         return array(get_class($this), $request->method());
     }
     
+    public function authenticate()
+    {
+        switch ($this->authentication)
+        {
+            case self::HTTP_BASIC:
+                require STATO_CORE_PATH.'/components/http_authentication/lib/basic_http_authentication.php';
+                return SBasicHttpAuthentication::authenticate($this->request, $this->response, $this->authentication_callback);
+                break;
+            default:
+                throw new SUnknownAuthenticationMethod("{$this->authentication} auth method unknown");
+        }
+    }
+    
     protected function responds($data, $status = 200)
     {
         $serializer = SAbstractSerializer::instantiate($this->format);
@@ -52,6 +75,17 @@ class SResource
         $this->response->body = $serializer->serialize($data);
         
         return $this->response;
+    }
+    
+    private static function resource_file($req_resource, $module)
+    {
+        if ($module === null)    
+            return STATO_APP_ROOT_PATH."/app/resources/{$req_resource}_resource.php";
+        
+        if (file_exists($path = STATO_APP_ROOT_PATH."/modules/{$module}/resources/base_resource.php"))
+            require_once($path);
+            
+        return STATO_APP_ROOT_PATH."/modules/{$module}/resources/{$req_resource}_resource.php";
     }
 }
 
