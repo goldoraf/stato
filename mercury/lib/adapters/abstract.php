566d8bb890e633b68f564edf8cad7d8f22a16371
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2008-03-31 12:35:56 +0000

    Timestamps are now casted to a timestamp column type instead of a datetime

--- mercury/lib/adapters/abstract.php
+++ mercury/lib/adapters/abstract.php
@@ -21,7 +21,7 @@ abstract class SAbstractAdapter
     protected $log  = array();
     protected $columns_cache   = array();
     protected $native_db_types = array();
-    
+
     public $runtime = 0;
     public $config = array
     (
@@ -31,30 +31,30 @@ abstract class SAbstractAdapter
         'pass'   => null,
         'dbname' => null
     );
-    
+
     abstract public function connect();
-    
+
     abstract public function disconnect();
-    
+
     abstract public function execute($sql);
-    
+
     abstract public function limit($count, $offset=0);
-    
+
     abstract public function query_columns($table);
-    
+
     abstract public function get_last_update($table);
-    
+
     public function __construct($config = array())
     {
         $this->config = array_merge($this->config, $config);
         $this->connect();
     }
-    
+
     public function select($sql)
     {
         return $this->execute($sql);
     }
-    
+
     public function select_all($sql)
     {
         $rs = $this->select($sql);
@@ -63,83 +63,83 @@ abstract class SAbstractAdapter
         $this->free_result($rs);
         return $set;
     }
-    
+
     public function select_one($sql)
     {
         return $this->fetch($this->select($sql));
     }
-    
+
     public function insert($sql)
     {
         if (!$this->execute($sql)) return false;
         return $this->last_insert_id();
     }
-    
+
     public function update($sql)
     {
         return $this->execute($sql);
     }
-    
+
     public function columns($table)
     {
         if (!isset($this->columns_cache[$table]))
             $this->columns_cache[$table] = $this->query_columns($table);
-            
+
         return $this->columns_cache[$table];
     }
-    
+
     public function reset_columns_cache()
     {
         $this->columns_cache = array();
     }
-    
+
     public function last_insert_id()
     {
         return $this->conn->last_insert_id();
     }
-    
+
     public function row_count($resource)
     {
         return $this->conn->row_count($resource);
     }
-    
+
     public function free_result($resource)
     {
         return $this->conn->free_result($resource);
     }
-    
+
     public function fetch($resource, $associative = true)
     {
         return $this->conn->fetch($resource, $associative);
     }
-    
+
     public function supports_transactions()
     {
         return $this->conn->supports_transactions();
     }
-    
+
     public function begin_transaction()
     {
         return $this->conn->begin_transaction();
     }
-    
+
     public function commit()
     {
         return $this->conn->commit();
     }
-    
+
     public function rollback()
     {
         return $this->conn->rollback();
     }
-    
+
     public function extract_length($sql_type)
     {
         preg_match('/\((.*)\)/', $sql_type, $matches);
         if (!empty($matches)) return $matches[1];
         return false;
     }
-    
+
     public function simplified_type($sql_type)
     {
         if (preg_match('/int/i', $sql_type))
@@ -150,14 +150,16 @@ abstract class SAbstractAdapter
             return SColumn::STRING;
         elseif (preg_match('/boolean/i', $sql_type))
             return SColumn::BOOLEAN;
-        elseif (preg_match('/datetime|timestamp/i', $sql_type))
+        elseif (preg_match('/datetime/i', $sql_type))
             return SColumn::DATETIME;
+        elseif (preg_match('/timestamp/i', $sql_type))
+            return SColumn::TIMESTAMP;
         elseif (preg_match('/date/i', $sql_type))
             return SColumn::DATE;
         elseif (preg_match('/float|double|decimal|numeric/i', $sql_type))
             return SColumn::FLOAT;
     }
-    
+
     // This method should be overriden when using PDO because PDO supports natively variables
     // binding in statements. Unfortunately, there was a bug in PDO until PHP 5.2.4 that
     // causes problems with placeholders and escaped quotes (http://bugs.php.net/bug.php?id=42113)
@@ -172,13 +174,13 @@ abstract class SAbstractAdapter
         }
         return $stmt;
     }
-    
+
     public function replace_bind_variables($stmt, $params)
     {
         foreach ($params as $value) $stmt = preg_replace('/\?/i', $this->quote($value), $stmt, 1);
         return $stmt;
     }
-    
+
     public function replace_named_bind_variables($stmt, $params)
     {
         foreach ($params as $key => $value)
@@ -188,7 +190,7 @@ abstract class SAbstractAdapter
         }
         return $stmt;
     }
-    
+
     public function quote($value, $attribute_type = Null)
     {
         if ($value === Null) return "NULL";
@@ -208,29 +210,29 @@ abstract class SAbstractAdapter
                 return $this->conn->quote_string($value);
         }
     }
-    
+
     public function quote_column_name($name)
     {
         return $name;
     }
-    
+
     public function array_quote($array)
     {
         foreach($array as $key => $value) $array[$key] = $this->quote($value);
         return $array;
     }
-    
+
     public function write_log()
     {
         $logger = SLogger::get_instance();
         foreach ($this->log as $log) $logger->debug($log);
     }
-    
+
     public function get_log()
     {
         return $this->log;
     }
-    
+
     protected function log($sql, $time, $name = null)
     {
         $this->log[] = (($name === null) ? 'SQL' : $name)
