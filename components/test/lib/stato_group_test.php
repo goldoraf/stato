90f6b1654c35adcf7513c828cf94e6cc437d5776
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2008-07-02 11:57:53 +0000

    Simplified StatoGroupTest class

--- components/test/lib/stato_group_test.php
+++ components/test/lib/stato_group_test.php
@@ -2,22 +2,8 @@
 
 class StatoGroupTest extends TestSuite
 {
-    private $tests_already_added = false;
-    
-    public function __construct($label = false)
-    {
-        parent::__construct($label);
-        if (($tests = $this->specificTestsToRun()) !== false)
-        {
-            $this->addTests($tests);
-            $this->tests_already_added = true;
-        }
-    }
-    
     public function addTestFolder($path)
     {
-        if ($this->tests_already_added) return;
-        
         set_include_path(get_include_path().PATH_SEPARATOR.$path);
         
         $it = new DirectoryIterator($path);
@@ -25,21 +11,11 @@ class StatoGroupTest extends TestSuite
             $this->addFile($elt->getFilename());
     }
     
-    private function addTests($tests)
+    public function addTestFile($path)
     {
-        $this->_label.= ': '.implode(', ', $tests);
-        
-        $tests_path = realpath(dirname($_SERVER['SCRIPT_NAME'])).'/test';
-        set_include_path(get_include_path().PATH_SEPARATOR.$tests_path);
-        foreach ($tests as $test) $this->addFile($test.'.test.php');
-    }
-
-    private function specificTestsToRun()
-    {
-        $args = $_SERVER['argv'];
-        array_shift($args); // skip file name
-        if (count($args) == 0) return false;
-        return $args;
+        $this->_label.= ': '.basename($path, '.php');
+        set_include_path(get_include_path().PATH_SEPARATOR.dirname($path));
+        $this->addFile(basename($path));
     }
 }
 
