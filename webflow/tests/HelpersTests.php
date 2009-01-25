8c77521e0703b8dc2ed7a170025c658ab03810c6
goldoraf <goldoraf@gmail.com>
2009-01-25 10:46:03 +0100

    Added number helpers

--- webflow/tests/HelpersTests.php
+++ webflow/tests/HelpersTests.php
@@ -3,6 +3,7 @@
 require_once dirname(__FILE__) . '/../../tests/TestsHelper.php';
 
 require_once 'helpers/StringHelperTest.php';
+require_once 'helpers/NumberHelperTest.php';
 
 class Stato_Webflow_HelpersTests
 {
@@ -10,6 +11,7 @@ class Stato_Webflow_HelpersTests
     {
         $suite = new PHPUnit_Framework_TestSuite('Stato webflow helpers');
         $suite->addTestSuite('Stato_StringHelperTest');
+        $suite->addTestSuite('Stato_NumberHelperTest');
         return $suite;
     }
 }
