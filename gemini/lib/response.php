2efd7c56869c9ce0ee0761bf6a607240e24c526e
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2009-04-01 09:00:29 +0000

    Removed status code text from the status header sent by SResponse class as it is in fact automatically added by PHP

--- gemini/lib/response.php
+++ gemini/lib/response.php
@@ -1,5 +1,7 @@
 <?php
 
+class SInvalidHttpStatusCode extends Exception {}
+
 class SResponse
 {
     public $body         = null;
@@ -9,62 +11,6 @@ class SResponse
     public $redirected_to = null;
     
     private static $default_headers = array('Cache-Control' => 'no_cache');
-    
-    public static $status_code_text = array(
-        100 => "Continue",
-        101 => "Switching Protocols",
-        102 => "Processing",
-        
-        200 => "OK",
-        201 => "Created",
-        202 => "Accepted",
-        203 => "Non-Authoritative Information",
-        204 => "No Content",
-        205 => "Reset Content",
-        206 => "Partial Content",
-        207 => "Multi-Status",
-        226 => "IM Used",
-        
-        300 => "Multiple Choices",
-        301 => "Moved Permanently",
-        302 => "Found",
-        303 => "See Other",
-        304 => "Not Modified",
-        305 => "Use Proxy",
-        307 => "Temporary Redirect",
-        
-        400 => "Bad Request",
-        401 => "Unauthorized",
-        402 => "Payment Required",
-        403 => "Forbidden",
-        404 => "Not Found",
-        405 => "Method Not Allowed",
-        406 => "Not Acceptable",
-        407 => "Proxy Authentication Required",
-        408 => "Request Timeout",
-        409 => "Conflict",
-        410 => "Gone",
-        411 => "Length Required",
-        412 => "Precondition Failed",
-        413 => "Request Entity Too Large",
-        414 => "Request-URI Too Long",
-        415 => "Unsupported Media Type",
-        416 => "Requested Range Not Satisfiable",
-        417 => "Expectation Failed",
-        422 => "Unprocessable Entity",
-        423 => "Locked",
-        424 => "Failed Dependency",
-        426 => "Upgrade Required",
-        
-        500 => "Internal Server Error",
-        501 => "Not Implemented",
-        502 => "Bad Gateway",
-        503 => "Service Unavailable",
-        504 => "Gateway Timeout",
-        505 => "HTTP Version Not Supported",
-        507 => "Insufficient Storage",
-        510 => "Not Extended"
-    );
     
     public function __construct()
     {
@@ -82,9 +28,10 @@ class SResponse
     
     public function send_headers()
     {
-        if (!isset($this->headers['Status']) && isset(self::$status_code_text[$this->status]))
-            $this->headers['Status'] = $this->status.' '.self::$status_code_text[$this->status];
-        header('HTTP/1.x '.$this->headers['Status']);
+        if (!is_int($this->status) || ($this->status < 100) || ($this->status > 599))
+            throw new SInvalidHttpStatusCode((string) $this->status);
+        
+        header('HTTP/1.x '.$this->status);
 		foreach($this->headers as $key => $value) header($key.': '.$value);
     }
     
