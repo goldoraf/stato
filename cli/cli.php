148cb48fbcad6964aaa841adad123c94e4027ec2
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2009-04-03 17:07:28 +0000

    Moved console component to stato core and renamed it as cli package final final step

---
+++ cli/cli.php
@@ -0,0 +1,5 @@
+<?php
+
+require 'lib/console_utils.php';
+require 'lib/code_generator.php';
+require 'lib/command.php';
--- cli/console.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require('lib/console_utils.php');
-require('lib/code_generator.php');
-require('lib/command.php');
-
-?>
