afe10f2bff5b037fcf0e091931bf206468427245
goldoraf <goldoraf@8b9fade5-25fc-c75f-3847-cbcaebcae28a>
2009-04-20 13:30:52 +0000

    Added set_include_path in the app skeleton boot file

--- webflow/lib/templates/createapp/boot.php
+++ webflow/lib/templates/createapp/boot.php
@@ -9,6 +9,8 @@ define('STATO_CORE_PATH', '<?php echo $project_core_path; ?>');
 define('STATO_APP_ROOT_PATH', str_replace('\\', '/', realpath(dirname(__FILE__).'/..')));
 define('STATO_ENV', ((isset($_SERVER['STATO_ENV'])) ? $_SERVER['STATO_ENV'] : 'development'));
 
+set_include_path(dirname(__FILE__).'/../app' . PATH_SEPARATOR . get_include_path());
+
 require(STATO_CORE_PATH.'/common/lib/initializer.php');
 
 SInitializer::boot();
