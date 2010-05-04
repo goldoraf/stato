50ae3062c32b73c3cd29f9d19d0cefaa87b0591b
vincenta <vincent.alquier@gmail.com>
2010-05-04 08:48:24 +0800

    the pgsql driver is now almost ready

--- library/Stato/Dbal/Dialect/Pgsql.php
+++ library/Stato/Dbal/Dialect/Pgsql.php
@@ -13,7 +13,7 @@ class Pgsql implements IDialect
     private static $columnTypes = array(
         'bigint'            => Column::INTEGER,
         'int8'              => Column::INTEGER, //alias de 'bigint'
-        'bigserial8'        => Column::INTEGER, //alias de 'bigint'
+        'bigserial'         => Column::INTEGER, //alias de 'bigint'
         'serial8'           => Column::INTEGER, //alias de 'bigint'
         //'bit',
         //'bit varying',
@@ -121,32 +121,82 @@ class Pgsql implements IDialect
 
     public function reflectTable(Connection $connection, $tableName)
     {
+        $columns = array();
+        $rs = $connection->execute("SELECT c.column_name, c.data_type, c.character_maximum_length, c.is_nullable, c.column_default, t.constraint_type"
+                                  ." FROM information_schema.columns c"
+                                  ." LEFT JOIN information_schema.key_column_usage k ON c.table_catalog = k.table_catalog"
+                                  ."           AND c.table_name = k.table_name AND c.column_name = k.column_name"
+                                  ." LEFT JOIN information_schema.table_constraints t ON t.constraint_name = k.constraint_name"
+                                  ."           AND t.constraint_type = 'PRIMARY KEY'"
+                                  ." WHERE c.table_name = '{$tableName}';");
+        foreach ($rs as $row) {
+            $options = array();
+            $name = $row['column_name'];
+            $type = $this->reflectColumnType($row['data_type']);
+            if ($type===Column::STRING) {
+                $options['length'] = $row['character_maximum_length'];
+            }
+            $options['nullable'] = ($row['is_nullable'] == 'YES') ? true : false;
+            if (substr($row['column_default'],0,7)=='nextval') {
+                $options['auto_increment'] = true;
+                $options['default'] = null;
+            } else {
+                $options['default'] = (!empty($row['Default'])) ? $row['Default'] : null;
+            }
+            $options['primary_key'] = ($row['constraint_type'] == 'PRIMARY KEY') ? true : false;
+            $columns[] = new Column($name, $type, $options);
+        }
+        return new Table($tableName, $columns);
     }
 
     public function createTable(Table $table)
     {
+        $columns = array();
+        $name = $table->getName();
+        foreach ($table->getColumns() as $column) $columns[] = $this->getColumnSpecification($column);
+        if ($pk = $table->getPrimaryKey()) $columns[] = "PRIMARY KEY (\"{$pk}\")";
+        $columns = implode(',', $columns);
+        return "CREATE TABLE \"{$name}\" ({$columns})";
     }
 
     public function truncateTable($tableName)
     {
-        return "TRUNCATE TABLE {$tableName}";
+        return "TRUNCATE TABLE \"{$tableName}\"";
     }
 
     public function dropTable($tableName)
     {
-        return "DROP TABLE {$tableName}";
+        return "DROP TABLE \"{$tableName}\"";
     }
 
     public function addColumn($tableName, Column $column)
     {
+        return "ALTER TABLE \"{$tableName}\" ADD COLUMN ".$this->getColumnSpecification($column);
     }
 
     public function getColumnSpecification(Column $column)
     {
+        $nativeType = self::$nativeTypes[$column->type];
+        $type = $nativeType['type'];
+        if ($column->type===Column::STRING) {
+            $length = (isset($nativeType['length'])) ? $nativeType['length'] : null;
+            $length = ($column->length !== null) ? $column->length : $length;
+        }
+        $length = (isset($length) && ($length !== null)) ? "({$length})" : '';
+        $default = ($column->default !== false) ? ' DEFAULT '.$this->getDefaultValue($column) : '';
+        $nullable = ($column->nullable === false) ? ' NOT NULL' : '';
+        if (($column->autoIncrement === true) && ($column->type === Column::INTEGER)) {
+            return "\"{$column->name}\" bigserial{$default}{$nullable}";
+        }
+        return "\"{$column->name}\" {$type}{$length}{$default}{$nullable}";
     }
 
     public function getDefaultValue(Column $column)
     {
+        if ($column->default === null && $column->nullable === true) return 'NULL';
+        if ($column->type == Column::BOOLEAN) return ($column->default === true ? '1' : '0');
+        if (($column->type == Column::INTEGER) || ($column->type == Column::FLOAT)) return $column->default;
+        return "'{$column->default}'";
     }
 
     private function reflectColumnType($sqlColumn)
